1609d95a9953049881f5f3ec6b6ec7e3ac9b0aec
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-05-27 22:26:04 -0700

    Generalize: create new entries as needed

--- src/OutlineToolkit.elm
+++ src/OutlineToolkit.elm
@@ -9,6 +9,7 @@ module OutlineToolkit exposing (Model, Msg, init, update, view)
 
 -}
 
+import Array exposing (Array)
 import Html exposing (Html, text)
 import Html.Attributes exposing (for, id)
 import Html.Events exposing (onInput)
@@ -16,32 +17,59 @@ import Html.Events exposing (onInput)
 
 {-| -}
 type Model
-    = Model ()
+    = Model
+        { entries : Array String
+        }
 
 
 {-| -}
 init : Model
 init =
-    Model ()
+    Model
+        { entries = Array.empty
+        }
+
+
+type alias Path =
+    Int
 
 
 {-| -}
 type Msg
-    = OnInputEntry String
+    = OnInputEntry Path String
 
 
 {-| -}
 update : Msg -> Model -> Model
-update msg model =
-    model
+update msg (Model model) =
+    Model <|
+        case msg of
+            OnInputEntry path newValue ->
+                { model
+                    | entries =
+                        model.entries
+                            |> createIfNecessary path
+                }
+
+
+createIfNecessary : Path -> Array String -> Array String
+createIfNecessary path array =
+    let
+        neededItems =
+            (path + 1) - Array.length array
+    in
+    if neededItems <= 0 then
+        array
+
+    else
+        Array.append array (Array.repeat neededItems "")
 
 
 {-| -}
 view : Model -> Html Msg
-view model =
+view (Model model) =
     Html.div []
-        [ viewEntryInput 1
-        , viewEntryInput 2
+        [ viewEntryInput (Array.length model.entries + 1)
         , text "Total: 10"
         ]
 
@@ -58,7 +86,7 @@ viewEntryInput i =
             [ text "New Entry" ]
         , Html.input
             [ id inputId
-            , onInput OnInputEntry
+            , onInput (OnInputEntry (i - 1))
             ]
             []
         ]
